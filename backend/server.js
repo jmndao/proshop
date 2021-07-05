a5fe2ef7cd9ef7c21ddc644c1e577a5581ec7090
Jonathan Moussa NDAO <jmndao@gmail.com>
2021-07-05 19:35:58 +0000

    Custom error Handling

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express"
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoutes";
+import { notFound, errorHandler } from "./middlewares/errorMiddlewares.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -15,6 +16,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
